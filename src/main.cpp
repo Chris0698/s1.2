87c6a3c780a57fe4aafb167aaa8ac522677d0c7d
Chris0698 <chris.aston98@outlook.com>
2018-02-12 23:46:00 +0000

    Updated with enums

--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,105 @@
 #include <mbed.h>
-
 #include <stdbool.h>
 
-int main() {
-    bool flashing = false;
+//only colors the LED supports
+enum LedColours
+{
+  red,
+  blue,
+  green
+};
 
-    DigitalOut red(PTB22);
-    DigitalIn  btn(PTC6);
+//The switches that can be used
+enum ButtonsSwitches
+{
+  rightButton,        //would have value of int =0
+  leftButton
+};
 
-    while(1) {
+//The buttons variables
+DigitalIn buttons[]
+{
+    DigitalIn(PTC6),//   0 button
+    DigitalIn(PTA4)
+};
+
+//LED variables
+DigitalOut LED[]
+{
+  DigitalOut(PTB22, 1),  //red
+  DigitalOut(PTB21, 1),  //blue
+  DigitalOut(PTE26, 1)   //green
+};
+
+bool isPressed(/*DigitalIn button*/ int press)
+{
+  //returns if the button been pressed.
+  return !buttons[press].read();
+}
+
+void ledOn(int i)
+{
+  LED[i].write(0);
+}
+
+void ledOff(int i)
+{
+  LED[i].write(1);
+}
+
+int main()
+{
+   LedColours color = blue;
+   ButtonsSwitches butSwitch = rightButton;
+   bool flashing = false;
+
+    //printf("button %d\n", leftButton.read() );
+
+    while(true)
+    {
+      if(isPressed(butSwitch))
+      {
+        flashing = !flashing;
+      }
+
+
+
+      if(flashing)
+      {
+        ledOn(color);
+        wait(0.5);
+        ledOff(color);
+        wait(0.5);
+      }
+      else
+      {
+        ledOff(color);
+        wait(0.5);
+      }
+      /*
+      flashing = isPressed(btn);
+
+      flashing = isPressed(center);
+
         if( btn.read() ) flashing = true;
             else         flashing = false;
+            */
 
-        printf("button %d\n", btn.read() );
-        if(flashing){
+        //If button not pressed then flashing = true
+        //printf("button %d\n", btn.read() );
+        /*
+        if(flashing)
+        {
             red.write(0);
             wait(0.5);
             red.write(1);
             wait(0.5);
-        }else{
+        }
+        else
+        {
             red.write(1);
             wait(1);
         }
+        */
     }
 }
